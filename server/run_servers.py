918aa054f71e288761543b345a68d7b4f12c4a7a
Yuan Shen <yshen47@illinois.edu>
2020-10-22 20:47:24 -0500

    save runnable version

--- server/run_servers.py
+++ server/run_servers.py
@@ -13,5 +13,5 @@ for i in range(args.num_servers):
     gpu_id = args.ids_gpus[i % len(args.ids_gpus)]
     port = args.starting_port + i*3
     cmd = "nvidia-docker run --rm -e NVIDIA_VISIBLE_DEVICES={} -p {}-{}:2000-2002 {} /bin/bash -c \"sed -i '5i sync' ./CarlaUE4.sh; ./CarlaUE4.sh /Game/Maps/Town01 -carla-server -benchmark -fps=10 -carla-settings=\"CarlaSettings.ini\"\"".format(gpu_id, port, port+2, args.image_name)
-    print i, cmd
+    print(i, cmd)
     subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
